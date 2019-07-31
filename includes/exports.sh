189bfbf1c056f90ba9f52c0beff28e4493bf1c3e
Anton Eprev <a.eprev@gmail.com>
2019-07-31 20:53:09 +0000

    Add LS_COLORS

--- includes/exports.sh
+++ includes/exports.sh
@@ -19,3 +19,4 @@ export HISTIGNORE="ls:cd:cd -:pwd:exit:date:* --help"
 
 # <3
 export GREP_COLORS="mt=38;5;208:sl=38;5;248:cx=:fn=38;5;72:ln=38;5;237:bn=:se=38;5;237"
+export LS_COLORS="di=38;5;111:fi=38;5;72:mi=38;5;167:ex=38;5;208:ln=:no=38;5;248"
